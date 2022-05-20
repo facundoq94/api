67e7d95aa90771ca928b95fd2e9b3f3c5c9bc14b
Facundo Quintana <facunquintana@gmail.com>
2022-05-19 22:31:25 -0300

    add vercel

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ const myconn = require('express-myconnection')
 const app = express()
 app.set('port', process.env.PORT || 9000)
 
+const dbOptions = {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'abc123',
+    database: 'replay'
+}
+
+//middleware
+//app.use(myconn(mysql, dbOptions, 'single'))
+
 //routes
 app.get('/', (req, res) => {
     res.send('welcome to my api gatoooi')
